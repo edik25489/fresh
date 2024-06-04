e37f38fa4e33b88d8f235ce72f471efe5fa92664
Eduard Yakovlev <edik25mas@yandex.ru>
2024-06-04 23:02:06 +0300

    04.06

---
+++ components/About.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-sheet
+      style="font-size: 50px;"
+      class="bg-transparent text-white mt-10 mb-4">
+    FRESH IS ABOUT
+  </v-sheet>
+  <v-sheet
+      class="mx-auto bg-transparent"
+  >
+    <v-slide-group
+        class="pa-4"
+        center-active
+        show-arrows
+        color="white"
+    >
+      <v-slide-group-item
+      >
+        <v-card
+            class="ma-4"
+            height="350"
+            width="200"
+        >
+          <div
+              style="background-position: center; background-size: cover; background-image: url(/image14.svg); background-repeat: no-repeat;"
+              class="d-flex fill-height flex-column align-center justify-center px-4">
+            <div>2</div>
+            <div>СТУДИИ</div>
+            <div>ДОБРАТЬСЯ ДО НАС ПРОЩЕ ПРОСТОГО</div>
+          </div>
+        </v-card>
+        <v-card
+            class="ma-4"
+            height="350"
+            width="200"
+
+        >
+          <div class="d-flex fill-height align-center justify-center">
+
+          </div>
+        </v-card>
+        <v-card
+            class="ma-4"
+            height="350"
+            width="200"
+
+        >
+          <div class="d-flex fill-height align-center justify-center">
+
+          </div>
+        </v-card>
+        <v-card
+            class="ma-4"
+            height="350"
+            width="200"
+
+        >
+          <div class="d-flex fill-height align-center justify-center">
+
+          </div>
+        </v-card>
+        <v-card
+            class="ma-4"
+            height="350"
+            width="200"
+
+        >
+          <div class="d-flex fill-height align-center justify-center">
+
+          </div>
+        </v-card>
+      </v-slide-group-item>
+    </v-slide-group>
+  </v-sheet>
+</template>
+
+<style scoped>
+
+</style>
